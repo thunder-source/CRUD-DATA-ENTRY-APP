6baebe0ff59bb504740128d0a068056ffebe7563
thunder-source <majhipraditya@gmail.com>
2022-11-22 23:37:22 +0530

    fix

--- src/components/Mui.js
+++ src/components/Mui.js
@@ -108,7 +108,7 @@ const Mui = () => {
 
   return alldata.setpopup ? (
     <>
-      <div className="justify-center items-center mt-[-80px] flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 bg-transparent">
+      <div className="justify-center items-center  flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 bg-transparent">
         <div className="relative  w-full mx-auto max-w-6xl">
           {/*content*/}
           <div className="border-0  rounded-lg relative flex flex-col w-full bg-transparent outline-none focus:outline-none">
